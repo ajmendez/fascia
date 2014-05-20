219ab466da064156bc08a3926b0d0e341378e304
Alexander Mendez <bluespace@gmail.com>
2014-05-20 00:45:57 -0700

    working popup

--- main.py
+++ main.py
@@ -11,43 +11,45 @@ from kivy.uix.gridlayout import GridLayout
 # from kivy.uix.textinput import TextInput
 
 
-# class MainScreen(FloatLayout):
-#   def calendar(self):
-#     for 
+class MainScreen(GridLayout):
+  def __init__(self, **kwargs):
+    self
+  # def calendar(self):
+  #   for 
 
 class FasciaApp(App):
   
   def build(self):
     today = datetime.now()
     cal = calendar.Calendar(calendar.SUNDAY)
-    layout = GridLayout(cols=7)
+    layout = GridLayout(cols=7, size_hint=(0.5,0.5))
     
     for iweek,week in enumerate(cal.monthdays2calendar(today.year, today.month)):
-      # label each col by the day
-      if iweek == 0:
+      if iweek == 0: # label each col by the day
         for _,wkday in week:
-          label = Label(text='[b]%s[/b]'%calendar.day_abbr[wkday])
+          label = Label(text='[b]%s[/b]'%calendar.day_abbr[wkday], markup=True)
           layout.add_widget(label)
       
-      # add each day
-      for day, wkday in week:
+      for day, wkday in week: # add each day
         if day != 0:
           button = Button(text='%d'%day)
         else:
-          button = Button(text = '')
-        
+          button = Label(text = '')
         layout.add_widget(button)
-      
-      # add as a popup
-      
-      self.popup = Popup(title='%d: %s'%(today.year,
-                                         calendar.month_abbr[today.month]),
-                         content = layout,
-                         size_hint = (None,None), 
-                         size = (400,500)
-                         )
->       # self.popup.open()
-    # return MainScreen()
+    
+    self.layout = layout
+    self.title = '%d: %s'%(today.year, calendar.month_abbr[today.month])
+    
+    self.popup = Popup(title='%d: %s'%(today.year,
+                                       calendar.month_abbr[today.month]),
+                       content = layout,
+                       size_hint = (0.535,0.535),
+                       # size_hint = (None,None),
+                       # size = (400,500)
+                       # size = layout.size,
+                       )
+    self.popup.open()
+    return layout
 
 if __name__ == '__main__':
     FasciaApp().run()
