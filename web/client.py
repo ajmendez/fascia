40955daaab02c3b36fe679d32c7561cf22cd8690
Alexander Mendez <bluespace@gmail.com>
2014-05-27 01:09:20 -0700

    adding weather resources, and server

--- web/client.py
+++ web/client.py
@@ -1,6 +1,8 @@
 ''' A simple website handler 
 2014 May : Mendez
 '''
+import sys
+import time
 try:
     import androidhelper as android
 except:
@@ -11,7 +13,11 @@ def main():
     '''Loads the website view'''
     droid = android.Android()
     try:
-        droid.webViewShow('http://hydrogen.bluenet.mooo.org')
+        droid.webViewShow('http://hydrogen.bluenet.mooo.com:5555')
+        while True:
+            time.sleep(10)
+            sys.stdout.write('.')
+            sys.stdout.flush()
     except KeyboardInterrupt:
         print 'Bye!'
     except:
--- web/server.py
+++ web/server.py
@@ -1,6 +1,11 @@
 '''server.py -- A simple tornado server
 2014.May : Mendez
 '''
+import os
+import sys
+import tornado.ioloop
+import tornado.web
+from datetime import datetime
 
 HOSTNAME = 'localhost'
 PORT = 5555
@@ -8,9 +13,41 @@ PORT = 5555
 
 
 
+class MainHandler(tornado.web.RequestHandler):
+    '''Handles the web requests'''
+    def get(self):
+        items = ['a','b']
+        time = datetime.now()
+        self.render('home.html', items=items, time=time)
+        # self.write("Hello, world")
+        # print 'client'
+        sys.stdout.write('.')
+        sys.stdout.flush()
+
+
 def server(hostname, port):
     '''Setup the server on this port'''
-
+    print 'Starting the Server!'
+    try:
+        mainpath = os.path.dirname(__file__)
+        settings = dict(
+            template_path = os.path.join(mainpath, "templates"),
+            debug = True,
+        )
+        
+        handlers = [
+            (r"/", MainHandler),
+            (r"/static/(.*)", tornado.web.StaticFileHandler, 
+                              {"path": os.path.join(mainpath,'static')}),
+        ]
+        
+        application = tornado.web.Application(handlers, **settings)
+        application.listen(port)
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print 'Bye!'
+    except:
+        raise
 
 if __name__ == '__main__':
     server(HOSTNAME, PORT)
