717b0111ae173bf87de45fa2f8ce0172fdda985c
Alexander Mendez <bluespace@gmail.com>
2014-05-31 02:10:23 -0700

    starting flickr

---
+++ web/flickr.py
@@ -0,0 +1,19 @@
+'''flickr module
+May 2014: Mendez
+'''
+
+
+class Flickr(object):
+  def __init__(self):
+    self.api = None
+  
+  def display(self):
+    pass
+  
+  def update(self):
+    pass
+  
+
+
+if __name__ == '__main__':
+  f = Flickr()
