245cf1724213e10a729e17d3dff4ecfbb5161a45
Alexander Mendez <bluespace@gmail.com>
2014-05-31 18:47:27 -0700

    starting on earth

---
+++ web/earth.py
@@ -0,0 +1,25 @@
+'''earth.py: get the clouds + earth image 
+May 2014 -- Mendez
+
+http://flatplanet.sourceforge.net/maps/night.html
+http://flatplanet.sourceforge.net/maps/natural.html
+http://wiki.birth-online.de/know-how/software/linux/xplanet
+
+http://mathematica.stackexchange.com/questions/3326/composition-how-to-make-a-day-and-night-world-map
+
+'''
+
+URL = 'http://xplanet.sourceforge.net.nyud.net:8080/clouds/tmp/201405310046.765843/clouds_2048.jpg'
+
+
+class Earth(object):
+    """Determine earth imaging"""
+    def __init__(self):
+        pass
+    
+    
+
+
+
+if __name__ == '__main__':
+    e = Earth()
