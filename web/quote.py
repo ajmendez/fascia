7c16d76b6edc0eca5d9254facd60475b819e337b
Alexander Mendez <bluespace@gmail.com>
2014-06-01 23:54:24 -0700

    adding quote module

---
+++ web/quote.py
@@ -0,0 +1,20 @@
+'''quote.py -- get a random quote
+May 2014 : Mendez
+
+'''
+
+
+
+
+
+class Quote(object):
+    def __init__(self):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    q = Quote()
+    q.display()
